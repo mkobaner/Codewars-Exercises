9ad43122667184bf4ddaa0244cf28903a057d4dc
M. Robert Kobaner <mkobaner@gmail.com>
2023-01-11 20:18:45 +0300

    Initial commit

---
+++ abbtwoword.js
@@ -0,0 +1,39 @@
+// function abbrevName(name){
+// let arrayedStr1=Array.from(name);
+//   console.log(arrayedStr1[0]);
+// let secondName=name.split(' ');
+//   console.log(secondName);
+// let arrayedStr2=Array.from(secondName[1]);
+// console.log(arrayedStr2);
+//   return(`${arrayedStr1[0].toUpperCase()}.${arrayedStr2[0].toUpperCase()}.`)
+//     // code away
+
+// }
+
+// abbrevName('Ali Baba');
+
+// function abbrevName(name){
+//     let arrayedStr1=Array.from(name);
+//       console.log(arrayedStr1[0]);
+//     let secondName=name.split(' ');
+//       console.log(secondName);
+//     let arrayedStr2=Array.from(secondName[1]);
+//     console.log(arrayedStr2);
+//       return(arrayedStr1[0].toUpperCase()+'.'+arrayedStr2[0].toUpperCase()+)
+//         // code away
+    
+//     }
+
+// function abbrevName(name){
+//     let arrayedStr1=Array.from(name);
+      
+//     let secondName=name.split(' ');
+     
+//     let arrayedStr2=Array.from(secondName[1]);
+    
+//       return(arrayedStr1[0].toUpperCase()+'.'+arrayedStr2[0].toUpperCase()+'.')
+//         // code away
+    
+//     }
+
+//     abbrevName('Ali Baba');
