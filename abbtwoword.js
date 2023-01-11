db0e3d3abc6c43542dcd393af7e4691d2b3f6c39
M. Robert Kobaner <mkobaner@gmail.com>
2023-01-12 01:38:18 +0300

    New solution for same problem
    I just studied arrays abit and than would like to challange myself for the second time and surprisingly same question asked so instead of giving my first answer, i decided to write code as short as possible, a two liner code might have been cleaner  tough.
    
    New answer starts at 58th line.

--- abbtwoword.js
+++ abbtwoword.js
@@ -37,3 +37,31 @@
 //     }
 
 //     abbrevName('Ali Baba');
+
+// function abbrevName(name){
+    
+//     return(name[0].to+'.'+name[name.indexOf(' ')+1])
+//      // code away
+
+// }
+
+
+// function abbrevName(name){
+    
+//     return((name[0].toUpperCase())+'.'+(name[(name.indexOf(' ')+1)].toUpperCase()))
+//      // code away
+
+// }
+
+// abbrevName('asnasdf fdfaef');
+
+function abbrevName(name){
+    //taking the first letter in name and than ill get index number of space add 1
+    //to it and  take it than i convert both letters to upper case and add a . to
+    //middle of them
+    
+      return((name[0].toUpperCase())+'.'+(name[(name.indexOf(' ')+1)].toUpperCase()))
+       // code away
+  
+  }
+  
