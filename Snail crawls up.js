3959de3947d71b2ebcc3fb04f7d93f67186e4eb2
M. Robert Kobaner <mkobaner@gmail.com>
2023-01-27 15:15:08 +0300

    Snail crawls up
    Ok this one looks easy :)

---
+++ Snail crawls up.js
@@ -0,0 +1,13 @@
+/* Snail crawls up https://www.codewars.com/kata/5b93fecd8463745630001d05/train/javascript
+The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+
+Your function takes three arguments:
+
+The height of the column (meters)
+The distance that the snail crawls during the day (meters)
+The distance that the snail slides down during the night (meters)
+Calculate number of day when the snail will reach the top of the column.*/
+
+function snail(column, day, night) {
+    // your code here
+  }
