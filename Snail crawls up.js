caf850d9372f79e1d51a658abf1a3806ef72cc40
M. Robert Kobaner <mkobaner@gmail.com>
2023-01-27 16:01:24 +0300

    Solved
    Well, well, well i think its safe to say my solution is cleanest and most elegant of the answers :)

--- Snail crawls up.js
+++ Snail crawls up.js
@@ -9,5 +9,10 @@ The distance that the snail slides down during the night (meters)
 Calculate number of day when the snail will reach the top of the column.*/
 
 function snail(column, day, night) {
-    // your code here
+    
+    return (Math.ceil((column-day)/(day-night)))+1 ;
+    // need to find out the days before reaching top cause that day snail wont be slipping say 25 5 3 snail wont come top in 13 days, instead in first 10 days it will reach 20 and than on 11'th day it will reach 25 and wont be slipping thats why there is(column-day)/(day-night) instead of column/(day-night).
+    
+
+
   }
