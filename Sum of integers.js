04549db6e1e7f381efa14d20ed687dc8c48169be
M. Robert Kobaner <mkobaner@gmail.com>
2023-01-29 14:12:25 +0300

    Update Sum of integers.js
    Added explanation about regex and difference between match and search.

--- Sum of integers.js
+++ Sum of integers.js
@@ -4,5 +4,12 @@ Your task in this kata is to implement a function that calculates the sum of the
 Note: only positive integers will be tested.*/
 
 function sumOfIntegersInString(s){
-    return // what the function name says
-  }
+    const result = s.match(/\d+/g)
+    if(result===null){
+        return 0;
+    }
+    else { return result.reduce((a,b)=> (+a)+(+b), +0)}
+    }
+
+/* Match searches for all items in string itdoesnt stop when it finds the first match, /\d is code for regex
+digits and g is global so this parameter searches for whole numbers /\w searches for words which is very useful.*/
