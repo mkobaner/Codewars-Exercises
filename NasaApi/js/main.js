045f3dac86a3755bb0bafa3e0b345a6aabbfa1c7
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-01 22:31:17 +0300

    Used Nasa api to get pictures and...
    explanations of the date given by the user.

--- NasaApi/js/main.js
+++ NasaApi/js/main.js
@@ -2,16 +2,23 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase();
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=7ASLXpwaPxOg7mybrVvZ1jQtEdDsBt8mDjN0dzUC'
+  const choice = document.querySelector('input').value;
+  console.log(choice);
+  const url = `https://api.nasa.gov/planetary/apod?api_key=7ASLXpwaPxOg7mybrVvZ1jQtEdDsBt8mDjN0dzUC&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('img').src=data.hdurl
+        document.querySelector('h3').innerText=data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
+
+
+
+
 
