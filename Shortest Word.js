c295f88b9ddaf84bebd22a874c7cfd7c1fe0ef6d
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-03 22:13:07 +0300

    Solved shortest World
    This kata is too easy sometimes, still looking other answers i learn alot .

--- Shortest Word.js
+++ Shortest Word.js
@@ -4,3 +4,29 @@ Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.*/
 
+
+function findShort(s){
+
+s=s.split(' ');
+// console.log(s);
+let low=s[0].length;
+s.forEach(element => {
+    if(low>element.length)
+    {
+        low=element.length;
+        // console.log(low);
+    }}
+    )
+return low;
+}
+
+// findShort('Mahmut bir kraldir Tamam mi adamim')
+
+
+/* this one looks much nicer :)
+
+function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
+
+*/
