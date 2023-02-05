debe2e0d9f4d594d876bc10ee1f9744559058bb2
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-05 23:23:34 +0300

    Solved and followed more of class 29

--- class29 leon solutions.js
+++ class29 leon solutions.js
@@ -23,9 +23,62 @@ function charmanderFights(myFah){
 
 //Loops
 //You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
-// numofPokemon=number(prompt('number of pokemons please'));
+numofPokemon=prompt('number of pokemons please');
 
-// for(x=1, x<=numofPokemon; x++) {
-//     console.log('Pikachu I choose you')
-// }
+for(x=1; x<=numofPokemon; x++) {
+    console.log('Pikachu I choose you')
+}
 
+//Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+function revOrder (arr){
+    return arr.reverse();
+}
+console.log(revOrder([1,2,3,4]));
+
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+function a2Biggerthanb3 (a,b){
+    let aTotal=0;
+    a.forEach(element => {
+        aTotal =aTotal+ element*element;
+    });
+    let bTotal=0;
+    b.forEach(element =>{
+        bTotal=bTotal+element*element*element;
+    })
+    return aTotal>bTotal;
+}
+a2Biggerthanb3([1,2,3],[1,2]);
+
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+
+function retMultiple (arr){
+    let newArray=[];
+    for(x=0; x<arr.length;x++){
+        if (arr[x]%x===0){
+            newArray.push(arr[x])
+        }
+    }
+    return newArray;
+}
+
+
+console.log(retMultiple([1,2,3,4,5,6,7,8]));
+
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+function sumAll (arr){
+    let total=0;
+    for(x=0; x<arr.length;x++){
+       total=total+ Number(arr[x])
+    }
+    return total;
+}
+console.log(sumAll(['2',3,4,5,'11']));
