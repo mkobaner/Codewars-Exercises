2b000ed58752e4f9e319fd9121863e77241e25bf
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-09 23:34:23 +0300

    Create Simple Fun #176 Reverse Letter.js

---
+++ Simple Fun #176 Reverse Letter.js
@@ -0,0 +1,23 @@
+/* Simple Fun #176: Reverse Letter https://www.codewars.com/kata/58b8c94b7df3f116eb00005b
+Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string*/
+
+function reverseLetter(str) {
+    let ans = str.replace(/[^a-zA-Z0-9]/g, '');
+    console.log(ans)
+    return ans.split('').reverse().join('');
+    
+  }
+
+  reverseLetter('ultr53o?n');
