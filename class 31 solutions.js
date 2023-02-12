ac435befc1b410b71d9da281c39a117c49de743c
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-12 21:33:36 +0300

    Update class 31 solutions.js
    made my own examples with rpg Characters used polymorphisim for attack where warrior attacks with str and weaponmastery, where archer attacks with range and arrow left methods.

--- class 31 solutions.js
+++ class 31 solutions.js
@@ -12,6 +12,10 @@ class Char{
     describe(){
         console.log(` ${this.name} has ${this.agi}, agi ${this.str} strengtg and ${this.con} `)
     }
+
+    attack(){
+       console.log(`${this.name} attacks`)
+    }
 }
 
 class Warrior extends Char{
@@ -20,5 +24,33 @@ class Warrior extends Char{
         this.weaponMastery=weaponMastery
         this.shieldMastery=shieldMastery
     }
+        attack(){
+            super.attack();
+            console.log(`deals damage with strength of ${this.str} and weaponmastery of ${this.weaponMastery}`)
+        }
+    }
+
+
+class Archer extends Char{
+    constructor(name, str, agi, con, range, arrows){
+        super(name,str,agi,con)
+        this.range=range
+        this.arrows=arrows
+    }
+        attack(){
+            super.attack();
+            console.log(`deals up to the range of ${this.range} and has  ${this.arrows} arrows left`)
+        }
+    }
+
+
+
+let Konan = new Warrior('Konan',17,15,16,22,45)
+let Mahmut = new Warrior('Mahmut',22,22,16,22,85)
+let Fatma = new Archer('Fatma',16,30,14,14,50)
+
+let group =[Konan,Mahmut,Fatma]
+for (member of group){
+    member.attack();
 }
-let Konan = new Warrior('Konan',17,15,16,45,25)
+
