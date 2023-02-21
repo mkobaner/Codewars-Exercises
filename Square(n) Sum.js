c24cb1aa51ffd9247489943abb663f59d1667d80
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-21 23:29:54 +0300

    Create Square(n) Sum.js

---
+++ Square(n) Sum.js
@@ -0,0 +1,31 @@
+/* Square(n) Sum https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+*/
+
+function squareSum(numbers){
+    let total=0;
+numbers.forEach(element => {
+    total=total+element**2
+});
+return total;
+}
+
+function squareSum2(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
+
+
+function mkx(numbers){
+    return numbers.reduce(function(sum,n){
+        return(n*n)+sum;
+    })
+}
+
+function mkm(numbers){
+    return numbers.reduce((sum,num)=> sum+(num*num),0)
+}
+//syntax of reduce needs some adjustment still!!!
+function MahmutX(numbers){
+    return numbers.redıce((sum,num)=> sum*sum-num +num+1)
+}
