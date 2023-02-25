55f525c2bbfb937e5b6ea533dcb6ad3f6a62c07c
M. Robert Kobaner <mkobaner@gmail.com>
2023-02-26 01:08:31 +0300

    Create Be Concise III - Sum Squares.js
    And solved and there is no better solution filter,reduce, ternaries all getting clearer

---
+++ Be Concise III - Sum Squares.js
@@ -0,0 +1,21 @@
+/* Be Concise III - Sum Squares https://www.codewars.com/kata/56f8fe6a2e6c0dc83b0008a7
+You are given a program sumSquares that takes an array as input and returns the sum of the squares of each item in an array. For example:
+
+sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+sumSquares([7,3,9,6,5]) === 200
+sumSquares([11,13,15,18,2]) === 843
+Shorten the code such that it meets the requirements.
+
+A few hints:
+
+Try researching about built-in Array methods; they may help shorten your code a lot
+Good luck!*/
+// function sumSquares(array) {
+//     var result = 0;
+//     for (let i = 0; i < array.length; i++) {
+//       result += array[i] ** 2;
+//     }
+//     return result;
+//   }
+
+  const sumSquares = arr => arr.reduce((acc,a)=> acc+a*a,0);
