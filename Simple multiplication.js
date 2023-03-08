812989efc961e5442e86d4d26ea63d5340ac9642
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-08 23:40:23 +0300

    Create Simple multiplication.js

---
+++ Simple multiplication.js
@@ -0,0 +1,4 @@
+/*Simple multiplication https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.*/
+
+let simpleMultiplication = n => n%2===0? n*8:n*9;
