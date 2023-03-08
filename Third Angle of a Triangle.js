72dbccaf32e7b12b1459660c0a3308762ec46733
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-08 23:35:50 +0300

    Create Third Angle of a Triangle.js

---
+++ Third Angle of a Triangle.js
@@ -0,0 +1,9 @@
+/*Third Angle of a Triangle https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.*/
+
+
+let otherAngle =(a,b) => 180-a-b;
