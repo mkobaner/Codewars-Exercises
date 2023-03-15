d5e9f752c19cd8d5b4d741d18f998e9e2e293e22
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-15 23:22:45 +0300

    Connected to MongoDB

--- crudApp/server.js
+++ crudApp/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser=require('body-parser')
+const MongoClient =require('mongodb').MongoClient
 const app=express();
+
+//parser first , before crud handlers
+app.use(bodyParser.urlencoded({extended:true}))
+
+//handlers here
 app.listen(3000,function(){
     console.log('listening on 3000')
 })
@@ -7,3 +14,15 @@ app.listen(3000,function(){
 app.get('/', function(req,res){
     res.sendFile(__dirname+'/index.html')
 })
+
+app.post('/quotes', (req, res) =>{
+    console.log(req.body)
+})
+
+
+
+MongoClient.connect('mongodb+srv://mkobaner1:akacaz@napoleon.d1ctoti.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+  })
+  .catch(error => console.error(error))
