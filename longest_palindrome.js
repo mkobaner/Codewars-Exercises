e34ebc181d2db5a2188171e994f9de0ba0fc45bd
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-15 20:58:11 +0300

    Create longest_palindrome.js
    Worked on it

---
+++ longest_palindrome.js
@@ -0,0 +1,20 @@
+/*longest_palindrome https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/javascript
+
+Longest Palindrome
+Find the length of the longest substring in the given string s that is the same in reverse.
+
+As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
+
+If the length of the input string is 0, the return value must be 0.
+
+Example:
+"a" -> 1 
+"aab" -> 2  
+"abcde" -> 1
+"zzbaabcd" -> 4
+"" -> 0 */
+
+let longestPalindrome=s=>{ 
+    let longP=1;
+    s.length==0?0:
+}
