988ea3a47993073c213431c4c453856a02b431c6
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-16 22:10:34 +0300

    Connected to MongoDB and created a collection

--- crudApp/server.js
+++ crudApp/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const bodyParser=require('body-parser')
-const MongoClient =require('mongodb').MongoClient
+const bodyParser=require('body-parser');
+const MongoClient =require('mongodb').MongoClient;
 const app=express();
 
+app.set('view engine', 'ejs');
 //parser first , before crud handlers
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
 //handlers here
 app.listen(3000,function(){
@@ -15,14 +16,32 @@ app.get('/', function(req,res){
     res.sendFile(__dirname+'/index.html')
 })
 
-app.post('/quotes', (req, res) =>{
-    console.log(req.body)
-})
+
 
 
 
 MongoClient.connect('mongodb+srv://mkobaner1:akacaz@napoleon.d1ctoti.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
+    const db=client.db('napoleon-quotes')
+    const quotesCollection=db.collection('quotes')
+    
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+    
+    app.get('/', (req, res) => {
+      db.collection('quotes').find().toArray()
+        .then(results => {
+          console.log(results)
+        })
+        .catch(error => console.error(error))
+      // ...
+    })
+
   })
-  .catch(error => console.error(error))
+  .catch(console.error)
