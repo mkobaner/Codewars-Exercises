211c442bf74fc65ae59db80fe0d99e0d38754521
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-17 20:47:49 +0300

    Create Remove exclamation marks.js

---
+++ Remove exclamation marks.js
@@ -0,0 +1,4 @@
+/*Remove exclamation marks  https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string. */
+
+removeExclamationMarks=s=> s.replace(/!/g,'')
