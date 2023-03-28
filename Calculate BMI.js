61614b466a43cc240e902c265fe513b0890842d3
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-28 23:38:19 +0300

    Solved and added interesting

--- Calculate BMI.js
+++ Calculate BMI.js
@@ -8,4 +8,11 @@ if bmi <= 25.0 return "Normal"
 if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese" */
-bmi=(w,h)=>
+bmi=(w,h)=>w/h**2<=18.5?'Underweight':w/h**2<=25?'Normal':w/h**2<=30?'Overweight':'Obese'
+
+
+// multiple lines ?
+
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
