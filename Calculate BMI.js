bb8d14d9d7b1afbfb530ceba5aa2847d8ea132c9
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-28 23:33:39 +0300

    Create Calculate BMI.js

---
+++ Calculate BMI.js
@@ -0,0 +1,11 @@
+/* Calculate BMI https://www.codewars.com/kata/57a429e253ba3381850000fb/train/javascript 
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese" */
+bmi=(w,h)=>
