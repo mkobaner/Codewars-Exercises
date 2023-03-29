a7676b40150934ba737880cf958a9cd93e06f21e
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-29 23:44:33 +0300

    Update Opposites Attract.js

--- Opposites Attract.js
+++ Opposites Attract.js
@@ -5,3 +5,12 @@ Timmy & Sarah think they are in love, but around where they live, they will only
 Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't. */
 
 lovefunc=(a,b)=>(a%2==0&&b%2==1)||(b%2==0&&a%2==1);
+
+//like the logic there
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+
+//lets do it with =>
+lovefunc=(a,b)=>a%2 !==b%2;
