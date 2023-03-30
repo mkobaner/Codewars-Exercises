66a1aa46237984718c28462ca6068cdeb8436593
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-30 14:26:04 +0300

    Create Odd-Even String Sort.js

---
+++ Odd-Even String Sort.js
@@ -0,0 +1,16 @@
+/*Odd-Even String Sort https://www.codewars.com/kata/580755730b5a77650500010c/train/javascript 
+ Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+
+Note: 
+0 is considered to be an even index. 
+All input strings are valid with no spaces */
+
+sortMystring=s=>{
+    let even=''
+    let odd=''
+    // even=s.split('').filter((n) => n%2==0).join('').toString();
+    // odd=s.split('').filter((n) => n%2!==0).join('').toString();
+    // return `${even} ${odd}`"
+}
+
+sortMystring('Mahmut Kobaner');
