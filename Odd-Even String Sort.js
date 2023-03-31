09533e2f102f02cdc57e83cb1c385096a1ffafba
M. Robert Kobaner <mkobaner@gmail.com>
2023-03-31 11:51:05 +0300

    Update Odd-Even String Sort.js

--- Odd-Even String Sort.js
+++ Odd-Even String Sort.js
@@ -5,12 +5,22 @@ Note:
 0 is considered to be an even index. 
 All input strings are valid with no spaces */
 
-sortMystring=s=>{
-    let even=''
-    let odd=''
-    // even=s.split('').filter((n) => n%2==0).join('').toString();
-    // odd=s.split('').filter((n) => n%2!==0).join('').toString();
-    // return `${even} ${odd}`"
+sortMyString=s=>{
+    return s.split('').reduce((acc,curr,i) =>{
+        i%2===0? acc[0] +=curr:acc[1] +=curr;
+        return acc;
+    },['', '']).join(' ');
 }
 
-sortMystring('Mahmut Kobaner');
+sortMyString('Mahmut Kobaner');
+
+// allright looks like if i used filter with index like this 
+// 
+sortMyString=s=>{
+    let even='';
+    let odd='';
+    even=s.split('').filter((n,i) => i%2==0).join('').toString();
+    odd=s.split('').filter((n,i) => i%2!==0).join('').toString();
+    return `${even} ${odd}`
+}
+sortMyString('Mahmut Kobaner');
