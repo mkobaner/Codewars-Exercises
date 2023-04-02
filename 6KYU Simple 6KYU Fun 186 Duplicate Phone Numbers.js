b966b009f2df1c57af3ef397233bb031a0b5cc39
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-02 23:19:56 +0300

    Update 6KYU Simple 6KYU Fun 186 Duplicate Phone Numbers.js

--- 6KYU Simple 6KYU Fun 186 Duplicate Phone Numbers.js
+++ 6KYU Simple 6KYU Fun 186 Duplicate Phone Numbers.js
@@ -47,4 +47,41 @@ The output should be:
 
 ["739-9425:6"]*/
 
+function findDuplicatePhoneNumbers(phoneNumbers) {
+  const keypadMapping = {
+    'A': 2, 'B': 2, 'C': 2,
+    'D': 3, 'E': 3, 'F': 3,
+    'G': 4, 'H': 4, 'I': 4,
+    'J': 5, 'K': 5, 'L': 5,
+    'M': 6, 'N': 6, 'O': 6,
+    'P': 7, 'R': 7, 'S': 7,
+    'T': 8, 'U': 8, 'V': 8,
+    'W': 9, 'X': 9, 'Y': 9,
+  };
+  
+  const standardPhoneNumbers = phoneNumbers.map(phoneNumber => {
+    let standardPhoneNumber = '';
+    for (let i = 0; i < phoneNumber.length; i++) {
+      const char = phoneNumber.charAt(i);
+      if (/\d/.test(char)) {
+        standardPhoneNumber += char;
+      } else if (/[A-Za-z]/.test(char)) {
+        standardPhoneNumber += keypadMapping[char.toUpperCase()];
+      }
+    }
+    return standardPhoneNumber.substring(0, 3) + '-' + standardPhoneNumber.substring(3);
+  });
+  
+  const countTable = {};
+  standardPhoneNumbers.forEach(phoneNumber => {
+    countTable[phoneNumber] = (countTable[phoneNumber] || 0) + 1;
+  });
+  
+  const duplicatePhoneNumbers = [];
+  Object.keys(countTable).forEach(phoneNumber => {
+    if (countTable[phoneNumber] > 1) {
+      duplicatePhoneNumbers.push(phoneNumber + ':' + countTable[phoneNumber]);
+    }
+  });
 
+  
