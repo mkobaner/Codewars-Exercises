5996c4d2b80ba6f7b00214b77a95e8902b07ac60
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-05 23:38:34 +0300

    Create Contamination #1 -String-.js

---
+++ Contamination #1 -String-.js
@@ -0,0 +1,16 @@
+/* Contamination #1 -String- 
+https://www.codewars.com/kata/596fba44963025c878000039/train/javascript
+n AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+STRINGSFUNDAMENTALS */
