b988c3a9291d00624390bd2a58c92c986410720d
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-07 21:33:47 +0300

    Solved
    Solved

--- Enumerable Magic 4 True for None.js
+++ Enumerable Magic 4 True for None.js
@@ -1,1 +1,11 @@
 /*Enumerable Magic #4 - True for None?
+
+*/
+
+const arr = [1, 3, 5, 7, 9];
+const isEven = num => num % 2 === 0;
+
+console.log(none(arr, isEven)); // Output: true
+
+const arr2 = [2, 4, 6, 8, 10];
+console.log(none(arr2, isEven)); // Output: false
