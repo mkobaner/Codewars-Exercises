84b2a0fa2ff9e3909e96ac2a2db71de9d4c3d37c
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-09 21:45:57 +0300

    Update Unfinished Loop - Bug Fixing #1.js
    solved

--- Unfinished Loop - Bug Fixing #1.js
+++ Unfinished Loop - Bug Fixing #1.js
@@ -1,1 +1,11 @@
-/* Unfinished Loop - Bug Fixing #1 
+/* Unfinished Loop - Bug Fixing #1 */
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
+  
