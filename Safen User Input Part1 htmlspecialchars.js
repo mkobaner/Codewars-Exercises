8095ee2958972bcf9fa118081cd15f743192b8c9
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-14 23:52:19 +0300

    Alternative solutions

--- Safen User Input Part1 htmlspecialchars.js
+++ Safen User Input Part1 htmlspecialchars.js
@@ -22,3 +22,27 @@ htmlspecialchars=n=>{
     }
     return result;
 }
+
+
+// there are multiple solutions 
+
+//replace method
+
+function htmlspecialchars(formData) {
+    return formData.replace(/&/g, "&amp;")
+                   .replace(/"/g, "&quot;")
+                   .replace(/</g, "&lt;")
+                   .replace(/>/g, "&gt;");
+  }
+
+// dictionary object
+
+function htmlspecialchars(formData) {
+    const dict = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;',
+    };
+    return formData.replace(/[<>"&]/g, char => dict[char]);
+}
