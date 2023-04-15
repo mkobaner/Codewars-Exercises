862bf63d98da017b9c3fd929e197fef3456da4d7
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-15 23:34:39 +0300

    Added 2 alternative solutions

--- Difference of Volumes of Cuboids.js
+++ Difference of Volumes of Cuboids.js
@@ -8,3 +8,17 @@ If you can, try writing it in one line of code. */
 
 findDifference=(a,b)=>a[0]*a[1]*a[2]>b[0]*b[1]*b[2]?a[0]*a[1]*a[2]-b[0]*b[1]*b[2]:b[0]*b[1]*b[2]-a[0]*a[1]*a[2]
 
+// or Math abs ifu know the basics u dont needs abs tough its just handy ...
+
+findDifference=(a,b)=> Math.abs(b[0]*b[1]*b[2]-a[0]*a[1]*a[2])
+
+// and than there is this one diffucult to read tough i stillget confused with reduce so not a bad example 
+
+function volume(c) {
+    return c.reduce((x, y) => x * y);
+  }
+  
+  function find_difference(a, b) {
+    return Math.abs(volume(a) - volume(b));
+  }
+
