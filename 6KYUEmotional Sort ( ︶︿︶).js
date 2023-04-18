cb7f286b06add4e37b60cfc0885558af9448d7ac
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-18 23:54:28 +0300

    Create 6KYUEmotional Sort ( ︶︿︶).js

---
+++ 6KYUEmotional Sort ( ︶︿︶).js
@@ -0,0 +1,66 @@
+/*Emotional Sort ( ︶︿︶) Emotional Sort ( ︶︿︶)
+You'll have a function called "sortEmotions" that will return an array of emotions sorted. It has two parameters, the first parameter called "arr" will expect an array of emotions where an emotion will be one of the following:
+
+:D -> Super Happy
+:) -> Happy
+:| -> Normal
+:( -> Sad
+T_T -> Super Sad
+Example of the array:[ 'T_T', ':D', ':|', ':)', ':(' ]
+
+And the second parameter is called "order", if this parameter is true then the order of the emotions will be descending (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
+
+Example if order is true with the above array: [ ':D', ':)', ':|', ':(', 'T_T' ]
+
+Super Happy -> Happy -> Normal -> Sad -> Super Sad
+If order is false: [ 'T_T', ':(', ':|', ':)', ':D' ]
+
+Super Sad -> Sad -> Normal -> Happy -> Super Happy
+Example:
+
+arr = [':D', ':|', ':)', ':(', ':D']
+sortEmotions(arr, true) // [ ':D', ':D', ':)', ':|', ':(' ]
+sortEmotions(arr, false) // [ ':(', ':|', ':)', ':D', ':D' ]
+More in test cases!
+
+Notes:
+
+The array could be empty, in that case return the same empty array ¯\_( ツ )_/¯
+All emotions will be valid
+Enjoy! (づ｡◕‿‿◕｡)づ */
+
+function sortEmotions(arr, order){
+    if(arr.length==0){
+        return []
+    } 
+    let superHappyCount=0;
+    let happyCount=0;
+    let normalCount=0;
+    let sadCount=0;
+    let superSadCount=0;
+
+    for (n=0;n<arr.length;n++){
+        arr[n]==':D'? superHappyCount+=1 :
+        arr[n]==':)'? happyCount+=1 :
+        arr[n]==':|'? normalCount+=1 :
+        arr[n]==':('? sadCount+=1 :
+        superSadCount+=1
+    }
+      // console.log(superHappyCount,happyCount,normalCount,sadCount,superSadCount,order) 
+
+    if (order==true) 
+    
+    {let result= [':D'.repeat(superHappyCount), ':)'.repeat(happyCount),':|'.repeat(normalCount),':('.repeat(sadCount),'T_T'.repeat(superSadCount)];
+
+    return result.filter((item)=>item !=='');
+
+}
+    
+    else 
+    {let result= ['T_T'.repeat(superSadCount),':('.repeat(sadCount),':|'.repeat(normalCount),':)'.repeat(happyCount),':D'.repeat(superHappyCount)]
+    return result.filter((item)=>item !==''); 
+}
+
+  }
+
+  sortEmotions(([ ':D', 'T_T', ':D', ':(' ], false));
