94858ec1c293af6aa5065e982ab73ea013020381
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-22 23:36:31 +0300

    Solved with regex

--- Did she say hallo.js
+++ Did she say hallo.js
@@ -17,4 +17,37 @@ you can assume the input is a string.
 to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 function should be case insensitive to pass the tests*/
 
-validateHello=g=>g.toLowerCase()==
+validateHello=g=>{
+    switch(g.toLowerCase()) {
+        case 'hello':
+        case 'ciao':
+        case 'salut':
+        case 'hallo':
+        case 'hola':
+        case 'ahoj':
+        case 'czesc':
+          return true;
+        default:
+          return false;
+      }
+}
+
+
+// tests wants to check for strings that contains the keywords
+
+validateHello=g=>{
+const greetings = ['hello', 'ciao', 'salut', 'hallo', 'hola', 'ahoj', 'czesc'];
+return greetings.includes(g.toLowerCase());
+}
+
+//this didnt work either
+
+function validateHello(input) {
+    const regex = /\b(hello|ciao|salut|hallo|hola|ahoj|czesc)\b/i;
+    return regex.test(input);
+  }
+
+  //works but lets do it without regex
+
+  
+
