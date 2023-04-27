ef5f0315f9f3883d8a7c8327e08079ea8717cfd4
M. Robert Kobaner <mkobaner@gmail.com>
2023-04-27 23:51:10 +0300

    Create Removing Elements.js

---
+++ Removing Elements.js
@@ -0,0 +1,12 @@
+/* Removing Elements https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/javascript
+ake an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+let removeEveryOther = arr=>{
+     arr.map((cur, index) => index!==0&&index%2==0?  arr.splice(index,1):'');
+    return arr;
+    };
